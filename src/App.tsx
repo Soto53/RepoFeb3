641a761f06705ad15e9cef72b7a8d6e6ed2b39f6
Tony <tonysoto915@gmail.com>
2025-02-03 11:30:59 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ArrayManipulator} from "./components/arrayMan";
+import {AddNumber} from "./components/numberInput";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Array Manipulation App</h1>
+        
       </header>
+      <AddNumber/>
+      <ArrayManipulator 
+      // numbersArray = {numbers}
+      // nameArray = {names}
+      />
     </div>
   );
 }
